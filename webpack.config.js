df295d1fd5f31c070a8b341263c48915de05230c
黄锴文 <huang19940214@163.com>
2020-07-16 17:55:10 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 module.exports = {
     mode:"development",
+    devtool: 'eval-source-map',
     entry: __dirname + "/src/main.js", //已多次提及的唯一入口文件
     output: {
         path: __dirname + "/dist",  //打包后的文件存放的地方
         filename: "bundle.js" //打包后输出文件的文件名
+    },
+    devServer:{
+        contentBase: './',   // 本地服务加载页面所在的目录
+        historyApiFallback: true,   // 路由跳转
+        inline: true,   // 实时刷新
     }
 }
 
